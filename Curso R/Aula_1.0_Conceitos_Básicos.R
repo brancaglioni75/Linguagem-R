dcc3f2d0aa3012550319cc0dd1de417df6a789db
Wilson Brancaglioni <70102493+brancaglioni75@users.noreply.github.com>
2024-04-01 22:13:43 -0300

    Add files via upload

---
+++ Curso R/Aula_1.0_Conceitos_Básicos.R
@@ -0,0 +1,85 @@
+## Primeiros comandos
+
+# Adição
+1+1
+
+# Subtração
+10-2
+
+# Multiplicação
+5*5
+
+# Divisão
+10/2
+
+# Raiz Quadrada
+sqrt(25)
+
+# Módulo ou valor absoluto
+abs(-100)
+
+# Logaritmo natural
+log(10)
+
+# Logaritmo na base 10
+log10(10)
+
+# Logaritmo na base 500
+log(500)
+
+# Logaritmando = 4 e Base = 2 o logaritmo eh 2
+log(4, base=2)
+
+# Exponencial
+exp(2)
+
+# Seno
+sin(0.5)
+
+# Cosseno
+cos(2)
+
+# Tangente
+tan(1)
+
+# Potencia de um numero qualquer. 3 elevado ao cubo = 27
+3 ^ 3
+
+
+# Para criar um objeto usamos <-
+
+x <- 10
+x
+a <- 1
+a
+
+x1 <- 4
+x1
+
+objeto <- 1
+meu_objeto <- 1
+meu.objeto <- 1
+
+# O que não podemos fazer quando nomeamos um objeto
+# 1x <- 1
+# meu-objeto <- 3
+# Diferencia letras minúsculas de maiúsculas
+
+# Funções
+# minha_funcao(arg1, arg2, argn)
+
+sum(2,3)
+sum(1:10)
+
+# Instalando pacotes
+
+# Tools, no proprio R
+# install.packages("nome_do_pacote")
+# Github: devtools::install_github("Nome_do_pacote")
+
+# if(!require(tidyverse))
+#   install.packages("tidyverse")
+
+# Carregando o tidyverse
+library(tidyverse)
+
